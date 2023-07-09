9322fb9e596b30ea901628079937124e08557d62
Zillur Rahman <76123185+zillur-web@users.noreply.github.com>
2023-07-09 16:47:07 +0600

    Update croppie.php

--- JavaScript/image_crop_to_upload/croppie.php
+++ JavaScript/image_crop_to_upload/croppie.php
@@ -1,1 +1,14 @@
 
+<?php
+$image = $_POST['image'];
+
+list($type, $image) = explode(';',$image);
+list(, $image) = explode(',',$image);
+
+$image = base64_decode($image);
+$image_name = time().'.png';
+file_put_contents('uploads/'.$image_name, $image);
+
+echo 'successfully uploaded';
+
+?>
